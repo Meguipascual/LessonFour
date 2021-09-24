d4f39f88f87a789367a053bc67100bab325e262b
meguipascual <meguipascual@gmail.com>
2021-09-24 17:21:33 +0200

    New enemy, new Physics material bouncy

---
+++ LessonFour/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float speed = .5f;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        enemyRb.AddForce(enemyRb.transform.forward*speed);
+        enemyRb.transform.LookAt(player.transform.position);
+    }
+}
